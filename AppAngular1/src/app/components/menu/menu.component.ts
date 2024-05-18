0db7ff009339d9dc199bd1b0e124d1a0ca69c22f
Javicorporation <krchipullajavier@gmail.com>
2024-05-17 22:39:02 -0500

    inificacion en el menu

--- AppAngular1/src/app/components/menu/menu.component.ts
+++ AppAngular1/src/app/components/menu/menu.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  rutas = [
+    {
+    name: 'Home',
+    path: '/home'
+    },
+    {
+    name: 'About',
+    path: '/about'
+    },
+    {
+    name: 'Contact',
+    path: '/contact'
+    },
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+    
 
 }
