4bafebafda0ac76eaa1dcc75f95bf78c672f97f6
Javicorporation <krchipullajavier@gmail.com>
2024-05-18 12:57:47 -0500

    Realizamos cambios en data.service.ts y creamos un metodo en app.module.ts

--- AppAngular1/src/app/pages/posts/posts.component.ts
+++ AppAngular1/src/app/pages/posts/posts.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.css'
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
+  constructor(){}
+  ngOnInit(): void {
+      
+  }
 
 }
