12b2a3c67cc82615373c59f4943e9851299601ff
Javicorporation <krchipullajavier@gmail.com>
2024-05-18 15:15:04 -0500

    nodificar posts.component.ts

--- AppAngular1/src/app/pages/posts/posts.component.ts
+++ AppAngular1/src/app/pages/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-posts',
@@ -6,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './posts.component.css'
 })
 export class PostsComponent implements OnInit {
-  constructor(){}
-  ngOnInit(): void {
-      
-  }
 
+  posts: any;
+  mensajes: any[] = [];
+
+
+  constructor(private dataService: DataService){}
+
+  ngOnInit(): void {
+
+      this.dataService.getPosts()
+        .subscribe(res => {
+          this.posts = res;
+
+          for (let i = 0; i < this.posts.length; i++) {
+            i < 10 ? this.mensajes.push(this.posts[i]) : void(0);
+            console.log(this.posts);
+          }
+        });
+  }
 }
